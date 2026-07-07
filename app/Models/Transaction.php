b8e5eec6433cddae733b82891942314b3901edf9
Nayrit <nayritarn@gmail.com>
2026-07-07 12:44:23 +0600

    models editted for database migrations

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'txn_id', 'type', 'sender_id', 'receiver_id', 'amount', 'fee'
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
-    //
+    // Protect against mass-assignment vulnerabilities
+    protected $fillable = ['user_id', 'balance'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
